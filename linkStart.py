83e208ff1965ad5d273430f99223f3b9b40f9274
Nick Waggoner <90349188+nswagg@users.noreply.github.com>
2022-05-01 15:59:26 -0600

    Multithreading

---
+++ linkStart.py
@@ -0,0 +1,49 @@
+"""
+    This is the main driver for the individual robot's subroutine. Each robot must be operated in the simulation
+    with its own connection to prevent ZeroMQ from deadlocking
+
+    NOTE::
+    ZMQ has a bug that causes sockets to freak if not enough time has passed between starting the threads
+"""
+
+from zmqRemoteApi.clients.python.zmqRemoteApi import RemoteAPIClient
+from dr20 import *
+import threading
+
+class subroutine:
+    def __init__(self, ID, robotName):
+        self.ID = ID
+        self.name = robotName
+        self.client = RemoteAPIClient() # establishes client connection through socket
+        self.sim = self.client.getObject('sim')
+        self.sim.addLog(self.sim.verbosity_scriptinfos, f"Initializing Subroutine {self.ID}")
+        self.threads = [] # Keeps track of the subroutines created threads. Should only be 1 thread
+
+
+    def startRoutine(self):
+        # Starts the socket connection
+        print(f"Starting Subroutine {self.ID}")
+        startSimulationEnv(self.sim)
+
+    def connectObject(self):
+        # Creates a thread for a single robot.
+        robot = dr20(self.sim, self.name, self.ID) # Initializes a new Robot object
+        # Setting Robot in motion with automated script
+        self.rThread = threading.Thread(target=robot.randomlyExploreForSetTime)
+        self.threads.append(self.rThread)
+        self.rThread.start()
+        sleep(.1)
+
+    def endRoutine(self):
+        # End the socket connection
+        for thread in self.threads:
+            thread.join()
+        print(f"Threads in routing {self.ID} rejoined")
+        sleep(1)
+        self.sim.stopSimulation()
+        print(f"Routine {self.ID} completed and exited.")
+
+    def driver(self):
+        # This is the threaded function.
+        self.startRoutine()
+        self.connectObject()
--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from HelperFunctions import *
-from zmqRemoteApi.clients.python.zmqRemoteApi import RemoteAPIClient
+"""
+    Main driver program for the CoppeliaSim Demo
+"""
+
 from time import *
-from dr20 import *
+from linkStart import *
 import threading
-NUM_ROBOTS = 4 # Defines how many threads we create
+NUM_ROBOTS = 2 # Defines how many threads we create based on the number of robots in the scene
 
 def main():
-    client = RemoteAPIClient()
-    sim = client.getObject('sim')
-    startSimulationEnv(sim)
+    threads = [] # Stores the simulation threads
 
     for i in range(NUM_ROBOTS):
+        """Setting up the connection threads for each robot"""
         robotName = "/dr20[" + str(i) + "]"
-        robot = dr20(sim, robotName, i)
-        thread = threading.Thread(target=robot.randomlyExploreForSetTime)
-        thread.start()
-
-    sleep(2)
-    sim.stopSimulation()
-
+        routine = subroutine(i, robotName)
+        simThread = threading.Thread(target=routine.driver())
+        threads.append(simThread)
+        print(f"Main: starting subroutine {i}")
+        sleep(.1)
 
 if __name__ == "__main__":
     main()
